f4bac850cc313673b1048df3e0a91563f174d8ac
milesthomas <ths22115@towerhill.org>
2025-10-13 23:09:23 -0400

    query client added w health endpoint implementation

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,34 @@ import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import About from './AboutUs';
 import Root from './Root';
 import Features from './Features';
-import Publications from "./Publications"
-import Calendar from "./Calendar"
+import Publications from "./Publications";
+import Calendar from "./Calendar";
 import './index.css';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 function App() {
+
+  const QUERY_STALE_TIME = 1000 * 60 * 30; // Data is 'fresh' for 30 mins
+  const QUERY_GC_TIME = 1000 * 60 * 90; // Unused queries are garbage collected after 90 mins
+
+  // Query client initalization
+  const queryClient = new QueryClient({
+    defaultOptions: {
+      queries: {
+        staleTime: QUERY_STALE_TIME, // Data is considered fresh for 5 minutes
+        gcTime: QUERY_GC_TIME, // Inactive queries are garbage collected after 1 hour
+        refetchOnWindowFocus: true,
+        retry: 2,
+      },
+      mutations: {
+        // Default options for mutations...
+      },
+    },
+  })
+
+
   return (
+    <QueryClientProvider client={queryClient}>
     <div>
       <BrowserRouter>
         <nav className = "flex justify-end gap-4 p-4">
@@ -25,6 +47,7 @@ function App() {
         </Routes>
       </BrowserRouter>
     </div>
+    </QueryClientProvider>
   );
 }
 
--- src/Root.tsx
+++ src/Root.tsx
@@ -1,12 +1,16 @@
 import { useState } from 'react'
 import './index.css'
+import { useGetHealth } from './api/queries'
 
 
 function Root() {
 
+  // Health endpoint destructuring
+  const { isSuccess, data, isError, error, isLoading }= useGetHealth();
+
   return (
     <div className = "p-4">
-      This is the root 
+      This is the root | server is {isSuccess && data.data || isLoading && 'loading...' || isError && 'unhealthy: ' + error.error}
     </div>
   )
 }
