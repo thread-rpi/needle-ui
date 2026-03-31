4d23d9a6f5f9ef4599bea5c265523d8bbce686ca
milesthomas <ths22115@towerhill.org>
2026-03-31 17:39:23 -0400

    added vitest for testing framework, updated vite config accordingly, modified API route for refresh token, and created test for modification

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
@@ -8,4 +8,8 @@ export default defineConfig({
     react(),
     tailwindcss(),
   ],
+  test: {
+    environment: 'node',
+    include: ['src/**/*.test.ts'],
+  },
 })
